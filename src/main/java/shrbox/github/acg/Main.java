53fb1fa16fd96316980045a0105ab78000a71e52
wdcszhwb <junxingliu2009@163.com>
2020-09-12 18:02:15 +0800

    接口请求添加随机数触发

--- src/main/java/shrbox/github/acg/Main.java
+++ src/main/java/shrbox/github/acg/Main.java
@@ -21,32 +21,25 @@ class Main extends PluginBase {
         }, 60 * 1000, 60 * 1000);
         System.setProperty("http.agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)");
         getEventListener().subscribeAlways(GroupMessageEvent.class, (GroupMessageEvent e) -> {
-            if (e.getMessage().contentToString().toLowerCase().equals("色图") || e.getMessage().contentToString().toLowerCase().equals("涩图")) {
+            String inputString = e.getMessage().contentToString().toLowerCase();
+            if (inputString.equals("色图") || inputString.equals("涩图") || inputString.equals("不够色") || inputString.equals("不够涩")) {
                 if (count > 15) {
                     e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender())).plus("一分钟内调用次数上限！"));
                     return;
                 }
                 count++;
-                NewThread newThread = new NewThread();
-                newThread.boot(e,"0");
-            }
-            if (e.getMessage().contentToString().toLowerCase().equals("r18")) {
-                if (count > 15) {
-                    e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender())).plus("一分钟内调用次数上限！"));
-                    return;
+                int random=(int)(Math.random()*10+1);
+                if(random == 2) {
+                    NewThread newThread = new NewThread();
+                    newThread.boot(e,"1");
+                }else if(random == 8){
+                    NewThread newThread = new NewThread();
+                    newThread.boot(e,"2");
+                }else {
+                    NewThread newThread = new NewThread();
+                    newThread.boot(e,"0");
                 }
-                count++;
-                NewThread newThread = new NewThread();
-                newThread.boot(e,"1");
-            }
-            if (e.getMessage().contentToString().toLowerCase().equals("混合")) {
-                if (count > 15) {
-                    e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender())).plus("一分钟内调用次数上限！"));
-                    return;
-                }
-                count++;
-                NewThread newThread = new NewThread();
-                newThread.boot(e,"2");
+
             }
 
         });
