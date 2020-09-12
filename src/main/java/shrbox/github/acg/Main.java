7fdbc60f7898a1572f71efa310af8a60a37c1e76
wdcszhwb <junxingliu2009@163.com>
2020-09-12 17:19:48 +0800

    修改请求判定状态码，修改入口触发条件

--- src/main/java/shrbox/github/acg/Main.java
+++ src/main/java/shrbox/github/acg/Main.java
@@ -21,7 +21,7 @@ class Main extends PluginBase {
         }, 60 * 1000, 60 * 1000);
         System.setProperty("http.agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)");
         getEventListener().subscribeAlways(GroupMessageEvent.class, (GroupMessageEvent e) -> {
-            if (e.getMessage().contentToString().toLowerCase().equals("色图")) {
+            if (e.getMessage().contentToString().toLowerCase().equals("色图") || e.getMessage().contentToString().toLowerCase().equals("涩图")) {
                 if (count > 15) {
                     e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender())).plus("一分钟内调用次数上限！"));
                     return;
